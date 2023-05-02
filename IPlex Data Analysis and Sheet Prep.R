2524b4956ffe8e924d16b20845b207ebd00511d6
Taylor K <84986254+etkellyedu@users.noreply.github.com>
2023-05-02 16:07:30 -0700

    Add files via upload

--- IPlex Data Analysis and Sheet Prep.R
+++ IPlex Data Analysis and Sheet Prep.R
@@ -10,6 +10,9 @@ p_unload(all)  # Easier: clears all add-ons
 rm(list=ls())
 # Clear mind :)
 
+library(pacman)
+
+
 
 library(dplyr)
 library(tidyr)
@@ -21,6 +24,7 @@ library(devtools)
 library(readr)
 library(readxl)
 library(stringr)
+library(plotly)
 
 # STEP 1: Turn genotype excel sheet into a csv ####
 # notes on manual manipulations of sheets
@@ -29,7 +33,7 @@ library(stringr)
 
 
 # Define the path to .xlsx files and the folder for the new csv sheets
-pathin <- "Data/RawIplex/"
+pathin <- "Data/RawIplex/NewFiles/"
 pathout <- "Data/GenotypeCSV/"
 
 # Get a list of all .xlsx files in the directory
@@ -82,6 +86,7 @@ for (csv_file in csv_files) {
 print(results_df)
 
 
+
 # STEP 2: Make genotype data file by combining all genotype CSVs ####
 # specify the directory path
 # old drive directory_path <- "Google Drive/Shared drives/Attardo Lab/Projects/Insecticide Resistance in Aedes aegypti/Data/iPlex Results /Iplex Genotype CSV/"
@@ -128,11 +133,21 @@ merged_data <- merged_data[, -c(2,3,26,27)]
 #some Ts get turned to TRUE (logical), change all to character
 merged_data[merged_data == "TRUE"] <- "T"
 
+# change all columns to characters to be sure
+merged_data <- merged_data %>% mutate_all(as.character)
+
 #replace blanks with NA
 merged_data <- merged_data %>% mutate_all(na_if,"")
 
+# clear all variables except for merged_data
+rm(list = ls(pattern = "[^merged_data]"))
+
+#write csv
+write.csv(merged_data, file = "IPlexMasterSheet_27Mar2023.csv")
 
 # STEP 3: PCA Sheet Prep ####
+combinedSNPs <- merged_data
+  
 combinedSNPs[combinedSNPs == "TRUE"] <- "T"
 combinedSNPs["X1000854266"][combinedSNPs["X1000854266"] == "T"] <- "1"
 combinedSNPs["X1055614166"][combinedSNPs["X1055614166"] == "T"] <- "1"
@@ -276,6 +291,145 @@ combinedSNPs["X3385240055"][combinedSNPs["X3385240055"] == "GA"] <- "2"
 combinedSNPs["vgsc_3315983611"][combinedSNPs["vgsc_3315983611"] == "TC"] <- "2"
 
 
+#manual adds
+
+SNPs_NAana["X1103349772"][SNPs_NAana["X1103349772"] == "T"] <- "1"
+SNPs_NAana["X1103349772"][SNPs_NAana["X1103349772"] == "TC"] <- "2"
+SNPs_NAana["X1103349772"][SNPs_NAana["X1103349772"] == "C"] <- "3"
+
+SNPs_NAana["X1188368236"][SNPs_NAana["X1188368236"] == "C"] <- "1"
+SNPs_NAana["X1188368236"][SNPs_NAana["X1188368236"] == "CT"] <- "2"
+SNPs_NAana["X1188368236"][SNPs_NAana["X1188368236"] == "T"] <- "3"
+
+
+combinedSNPs["X1103349772"][combinedSNPs["X1103349772"] == "T"] <- "1"
+combinedSNPs["X1103349772"][combinedSNPs["X1103349772"] == "TC"] <- "2"
+combinedSNPs["X1103349772"][combinedSNPs["X1103349772"] == "C"] <- "3"
+
+combinedSNPs["X1188368236"][combinedSNPs["X1188368236"] == "C"] <- "1"
+combinedSNPs["X1188368236"][combinedSNPs["X1188368236"] == "CT"] <- "2"
+combinedSNPs["X1188368236"][combinedSNPs["X1188368236"] == "T"] <- "3"
+
+PCAsheet <- left_join(combinedSNPs, samplemeta)
+
+# Filter out rows that contain "none" or "Florida" in a specific column
+# Creating a list of values to filter by
+filter_values <- c(0,1,2,3,4,5,6,7,8,10,11)
+filter_values <- c(9,12,13,14,15,16,17,18,19,20,21,22,23,25,26,27,28,29,30,31)
+# Using filter() function from dplyr package
+
+PCA_filtered <- PCAsheet %>% filter(PopIDforsorting %in% filter_values)
+
+#STEP 3A: Run PCA ####
+#PCA comp
+#How many columns? 47 
+ncol(PCA_filtered)
+
+
+#Make sure there are NO NAs in the annotation columns! Or else they will be dropped
+
+#filter out features in STEP 5
+SNPs<- PCA_filtered
+
+SNPs_NA<-na.omit(SNPs)
+SNPs_NAana <- left_join(SNPs_NA, samplemeta)
+# check if data is numeric
+is.numeric(SNPs_NAana[c(2:35)])
+# convert column 2 to 35 to double
+SNPs_NAana[, 2:35] <- lapply(SNPs_NAana[, 2:35], as.double)
+#filter 
+
+write.csv(SNPs_NAana, "Data/SNPsNAanaNewIntroLA")
+results <- prcomp(SNPs_NAana[c(2:35)])
+
+summary(results)
+
+#PCAs
+screeplot(results, type = "l", npcs = 15, main = "Screeplot of the first 10 PCs")
+abline(h = 1, col="red", lty=5)
+legend("topright", legend=c("Eigenvalue = 1"),
+       col=c("red"), lty=5, cex=0.6)
+cumpro <- cumsum(results$sdev^2 / sum(results$sdev^2))
+plot(cumpro[0:15], xlab = "PC #", ylab = "Amount of explained variance", main = "Cumulative variance plot")
+abline(v = 6, col="blue", lty=5)
+abline(h = 0.88759, col="blue", lty=5)
+legend("topleft", legend=c("Cut-off @ PC6"),
+       col=c("blue"), lty=5, cex=0.6)
+
+
+plot(results$x[,1],results$x[,2], xlab="PC1 (25%)", ylab = "PC2 (8%)", main = "PC1 / PC2 - plot")
+
+
+library("factoextra")
+fviz_pca_ind(results, geom.ind = "point", pointshape = 21, 
+             pointsize = 2, 
+             fill.ind = SNPs_NAana$SampleArea, 
+             col.ind = "black", 
+             palette = "jco", 
+             addEllipses = FALSE,
+             label = "var",
+             col.var = "black",
+             repel = TRUE,
+             legend.title = "Population") +
+  ggtitle("2D PCA-plot") +
+  theme(plot.title = element_text(hjust = 0.5))
+
+#Plotly PCA
+#try to make a plot in plotly 
+explained_variance_ratio <- summary(results)[["importance"]]['Proportion of Variance',]
+explained_variance_ratio <- 100 * explained_variance_ratio
+components <- results[["x"]]
+components <- data.frame(components)
+components <- cbind(components, SNPs_NAana$BayGroup )
+components$PC3 <- -components$PC3
+components$PC2 <- -components$PC2
+tot_explained_variance_ratio <- summary(results)[["importance"]]['Proportion of Variance',]
+tot_explained_variance_ratio <- 100 * sum(tot_explained_variance_ratio)
+tit = 'Total Explained Variance = 39.61'
+
+fig3D <- plot_ly(components, x = ~PC1, y = ~PC2, z = ~PC3, color = ~SNPs_NAana$BayGroup ) %>%
+  add_markers(size = 12)
+
+fig <- components %>%
+  plot_ly()  %>%
+  add_trace(
+    type = 'splom',
+    dimensions = list(
+      list(label=paste('PC 1 (',toString(round(explained_variance_ratio[1],1)),'%)',sep = ''), values=~PC1),
+      list(label=paste('PC 2 (',toString(round(explained_variance_ratio[2],1)),'%)',sep = ''), values=~PC2),
+      list(label=paste('PC 3 (',toString(round(explained_variance_ratio[3],1)),'%)',sep = ''), values=~PC3),
+      list(label=paste('PC 4 (',toString(round(explained_variance_ratio[4],1)),'%)',sep = ''), values=~PC4)),
+    color = ~SNPs_NAana$BayGroup) %>%
+  style(diagonal = list(visible = FALSE)) %>%
+  layout(
+    legend=list(title=list(text='color')),
+    hovermode='closest',
+    dragmode= 'select',
+    plot_bgcolor='rgba(240,240,240, 0.95)',
+    xaxis=list(domain=NULL, showline=F, zeroline=F, gridcolor='#ffff', ticklen=4),
+    yaxis=list(domain=NULL, showline=F, zeroline=F, gridcolor='#ffff', ticklen=4),
+    xaxis2=axis,
+    xaxis3=axis,
+    xaxis4=axis,
+    yaxis2=axis,
+    yaxis3=axis,
+    yaxis4=axis )
+
+fig
+fig3D
+
+#STEP 7 PED File prep####
+#MAP/PED file prep
+
+#merged_data
+# Apply gsub() function to columns 2:4
+merged_data[, 2:42] <- apply(merged_data[, 2:42], 2, function(x) {
+  gsub("(?<!.)\\b(\\w)\\b(?!\\.)", "\\1\\1", x, perl = TRUE)
+})
+
+PEDfile<-  left_join(merged_data, samplemeta)
+
+write.csv(PEDfile, "Data/Apr11pedfile.csv")
 # STEP 4: Structure Sheet Prep ####
 # Use `str_length` from `stringr` to determine the number of characters in each cell
 # If the number of characters is 2, keep only the first character
@@ -286,8 +440,8 @@ drop_second_letter <- function(i) {
   # Create a new data frame to store the modified data
   data_mod <- i
   
-  # Loop through each column of the data frame, starting from the third column
-  for (col_name in names(data_mod)[3:ncol(data_mod)]) {
+  # Loop through each column of the data frame, starting from the second column
+  for (col_name in names(data_mod)[2:ncol(data_mod)]) {
     # Use the `str_length` function from the `stringr` library to get the length of each cell in the column
     cell_lengths <- str_length(data_mod[, col_name])
     
@@ -304,8 +458,8 @@ drop_first_letter <- function(i) {
   # Create a new data frame to store the modified data
   data_mod <- i
   
-  # Loop through each column of the data frame, starting from the third column
-  for (col_name in names(data_mod)[3:ncol(data_mod)]) {
+  # Loop through each column of the data frame, starting from the second column
+  for (col_name in names(data_mod)[2:ncol(data_mod)]) {
     # Use the `str_length` function from the `stringr` library to get the length of each cell in the column
     cell_lengths <- str_length(data_mod[, col_name])
     
@@ -323,6 +477,11 @@ SNP_2 <- drop_first_letter(merged_data)
 #Bind together 
 StructureSheet_Date <- bind_rows(SNP_1, SNP_2)
 
+#merge with metadata
+StructureSheet_WMeta <- left_join(StructureSheet_Date, samplemeta)
+
+#write csv
+write.csv(StructureSheet_WMeta, "Data/Structure_Apr12023.csv")
 
 #NOTES from datalab meeting ####
 #PCA- wide data, matrix 
@@ -359,7 +518,12 @@ for (i in 1:nrow(SNPcode)){
 #STEP 5: Call Rate Evaluation ####
 
 # data is wide, make it long 
-long_allele <- pivot_longer(merged_data, cols = -1, names_to = "SNP", values_to = "Genotype")
+MergedAna<- left_join(merged_data, samplemeta)
+long_alleleA <- pivot_longer(MergedAna, cols = -c(1,43:47), names_to = "SNP", values_to = "Genotype")
+
+#PCA long allel
+long_allele <- pivot_longer(PCA_filtered, cols = -c(1,43:47), names_to = "SNP", values_to = "Genotype")
+long_allele <- long_allele %>% select(-c(3,4))
 
 #calculate SNP Call Rate Per Sample
 Sample_CallRate <- long_allele %>% 
@@ -375,7 +539,7 @@ TotalSample <- unique(long_allele$SAMPLE_NAME)  %>%
 SNP_CallRate <- long_allele %>% 
   group_by(SNP) %>%
   summarize(n_NA = sum(is.na(Genotype))) %>% 
-  mutate(Call = (1-(n_NA/674))*100) 
+  mutate(Call = (1-(n_NA/836))*100) 
 
 # lookup table questions
 #check its a data frame
@@ -392,10 +556,16 @@ SNP_CallRate %>%
 SNindex <- SNP_CallRate$SNP[SNP_CallRate$Call>90]
 filteredcol <- select(merged_data, SAMPLE_NAME, all_of(SNindex))
 
+#update sheet 
+PCA_filtered <- select(PCA_filtered, SAMPLE_NAME, all_of(SNindex))
+
 #filter rows
-SAMindex <- Sample_CallRate$SAMPLE_NAME[SNP_CallRate$Call>70]
+SAMindex <- Sample_CallRate$SAMPLE_NAME[SNP_CallRate$Call>97]
 #filtering rows here, not searching by name. Instead search by some logical test, like is it within samindex
 filteredSAM <- filter(filteredcol, SAMPLE_NAME %in% SAMindex)
+
+#new sheet 
+PCA_filtered <- filter(PCA_filtered, SAMPLE_NAME %in% SAMindex)
 
 # STEP 6: Allele Frequency Tables ####
 
@@ -406,13 +576,16 @@ SNPcode <- read.csv("Data/VGSCSNPCode.csv") %>%
                names_to = "Phenotype", 
                values_to = "Genotype")
 
+#read in sample metadata
+samplemeta <- read.csv("Data/IPlexMetaData.csv")
+
 #Make VGSC SNPs list for search
 VGSC_SNPs <- c('vgsc_3315931548','vgsc_3315931672', 'vgsc_3315983611',
          'vgsc_3315983763','vgsc_3315999297' ,'vgsc_3316014588',
          'vgsc_3316080722','X3315939224')
 
 #filter by VGSC list
-Allele_VGSC <- long_allele %>% 
+Allele_VGSC <- long_alleleA %>% 
   filter(SNP %in% VGSC_SNPs) %>%
   count(Genotype, SNP) 
 
@@ -422,12 +595,34 @@ Allele_VGSC$Genotype  <- replace_na(Allele_VGSC$Genotype, "No Call")
 #Merge VGSC Key and filtered IPlex Data 
 VGSC_table <- merge(Allele_VGSC, SNPcode)
 
+#Merge metadata info
+VGSC_table2 <- merge(VGSC_table, samplemeta)
 
 #Pivot Wider
 VGSCWide <- VGSC_table %>% select(3:5) %>%
   pivot_wider(names_from = Musca,
                            values_from= n)
   
+#Pivot Wider
+VGSCWide <- VGSC_table2 %>% select(3:5,6,9) %>%
+  pivot_wider(names_from = Musca,
+              values_from= n)
 
 
+#Population grouping 
+long_alleleMeta <- left_join(long_alleleA, samplemeta)
+MetaSNP <- left_join(long_alleleMeta, SNPcode) %>% 
+  filter(SNP %in% VGSC_SNPs)
 
+
+Counts <- MetaSNP %>% 
+  group_by(SampleArea) %>%
+  count(Genotype, SNP, Phenotype, Musca)
+
+widecounts <- Counts %>% pivot_wider(names_from = Musca,
+                                                 values_from= n)
+
+
+together <-left_join(merged_data, samplemeta)
+
+write.csv(together)
